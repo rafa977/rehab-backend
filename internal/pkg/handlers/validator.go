876ced02f702c9c4d6bf29b5f5869ac7a53642e8
Rafa <rkiloudi@intrasoft-intl.com>
2023-01-17 10:29:51 +0200

    Added validator central function for inputs

---
+++ internal/pkg/handlers/validator.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validate *validator.Validate
+
+func ValidateInputs(dataSet interface{}) (bool, map[string]string) {
+
+	var validate *validator.Validate
+
+	validate = validator.New()
+
+	err := validate.Struct(dataSet)
+
+	if err != nil {
+		if err, ok := err.(*validator.InvalidValidationError); ok {
+			panic(err)
+		}
+
+		errors := make(map[string]string)
+		reflected := reflect.ValueOf(dataSet)
+
+		for _, err := range err.(validator.ValidationErrors) {
+			field, _ := reflected.Type().FieldByName(err.StructField())
+
+			var name string
+			if name = field.Tag.Get("json"); name == "" {
+				name = strings.ToLower(err.StructField())
+			}
+
+			switch err.ActualTag() {
+			case "required":
+				errors[name] = "Required " + name
+				break
+
+			case "email":
+				errors[name] = "Email must be in the following format: mail@example.com"
+				break
+			}
+
+			return false, errors
+		}
+	}
+	return true, nil
+}
--- internal/pkg/models/account.go
+++ internal/pkg/models/account.go
@@ -5,14 +5,14 @@ import "github.com/uptrace/bun"
 type Account struct {
 	bun.BaseModel `bun:"table:accounts,alias:ac"`
 
-	UserID    string `bun:",autoincrement"`
-	Username  string `bun:"username,notnull"`
-	Password  string `bun:"password,notnull"`
-	Firstname string
-	Lastname  string
-	Email     string
-	Amka      string
-	Age       string
+	UserID    string `bun:",autoincrement" `
+	Username  string `bun:"username, notnull" json:"username" validate:"required"`
+	Password  string `bun:"password, notnull" json:"password" validate:"required"`
+	Firstname string `bun:"firstname, notnull" json:"firstname" validate:"required"`
+	Lastname  string `bun:"lastname, notnull" json:"lastname" validate:"required"`
+	Email     string `bun:"email, notnull" json:"email" validate:"required,email,max=128"`
+	Amka      string `bun:"amka, notnull" json:"amka" validate:"required"`
+	Age       string `bun:"age, notnull" json:"age" validate:"required"`
 	Job       string
 	CreatedOn string
 	LastLogin string
