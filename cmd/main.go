035022adfdd2a81b9350df02a1fb5246dc9bc8b8
rafail <r.kiloudis@gmail.com>
2023-02-07 21:16:44 +0200

    Updates & Keycloak

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,48 @@
 package main
 
 import (
-	_ "github.com/lib/pq"
+	"context"
 
+	"github.com/Nerzal/gocloak/v12"
 	"github.com/rehab-backend/api/accounts"
 	"github.com/rehab-backend/internal/pkg/handlers"
 )
 
 func main() {
+
+	client := gocloak.NewClient("http://localhost:8080/auth/")
+	ctx := context.Background()
+
+	token, err := client.LoginClient(ctx, "rehab-go", "h5rQCqL7dgofp4OdCBZOFUVBxWJXRNBC", "Rehab")
+	if err != nil {
+		panic("Login failed:" + err.Error())
+	}
+
+	// token, err := client.LoginAdmin(ctx, "admin", "admin", "Rehab")
+	// if err != nil {
+	// 	panic("Something wrong with the credentials or url")
+	// }
+
+	// var params gocloak.GetUsersParams
+	// params.Username = "admin"
+
+	// _, err = client.GetUsers(ctx, token.AccessToken, "Rehab", params)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	user := gocloak.User{
+		FirstName: gocloak.StringP("Bob"),
+		LastName:  gocloak.StringP("Uncle"),
+		Email:     gocloak.StringP("something@really.wrong"),
+		Enabled:   gocloak.BoolP(true),
+		Username:  gocloak.StringP("CoolGuy"),
+	}
+
+	_, err = client.CreateUser(ctx, token.AccessToken, "Rehab", user)
+	if err != nil {
+		panic(err.Error())
+	}
 
 	// dbConnection := config.ConnectDB()
 
